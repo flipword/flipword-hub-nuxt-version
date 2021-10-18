dc75e9669eae3dd3386c1b058075a56c23703f27
acroquelois <acroquelois@WAITLADCP01W.entreprise.access-it.net>
2021-10-18 23:12:38 +0200

    :tada: initialize repo

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="mt-2">Hello Word !</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Index',
+  setup() {},
+})
+</script>
