b68212aa3878de6549e255d90a3cd89aa7f720da
Croquelois Adrien <croquelois.adrien@gmail.com>
2021-10-26 23:37:27 +0200

    :sparkles: add title

--- pages/[lang].vue
+++ pages/[lang].vue
@@ -1,21 +1,81 @@
 <template>
-  <div class="flex justify-between">
-    <span class="text-4xl">Flipword tell you: {{ i18n("greeting") }}</span>
-    <select
-      v-if="currentLang"
-      v-model="currentLang"
-      name="lang"
-      @change="updateLang"
-    >
-      <option
-        v-for="(lang, index) in langOptions"
-        :key="index"
-        :value="lang.id"
-        :selected="currentLang == lang.id"
-      >
-        {{ lang.label }}
-      </option>
-    </select>
+  <div>
+    <div class="screen-part1 bg-primary flex flex-row">
+      <div class="w-1/2 flex justify-center">
+        <div class="h-3/5 w-3/4 relative">
+          <div
+            class="
+              w-full
+              top-0
+              -right-2
+              -bottom-1
+              bg-whiteShadow
+              rounded-b-5xl
+              absolute
+            "
+          ></div>
+          <div
+            class="
+              h-full
+              w-full
+              bg-white
+              rounded-b-5xl
+              relative
+              flex
+              justify-center
+              items-center
+            "
+          >
+            <div class="h-5/6 w-5/6 bg-primary rounded-b-5xl">
+              <div
+                class="
+                  w-full
+                  h-full
+                  pb-20
+                  flex flex-col
+                  justify-center
+                  items-center
+                "
+              >
+                <div class="w-full flex flex-row justify-center">
+                  <img
+                    src="assets/icons/logo.png"
+                    style="height: 120px; width: 120px"
+                  />
+                  <span class="text-9xl text-black">FlipWord</span>
+                </div>
+                <div class="w-full flex justify-center mt-2">
+                  <span class="text-4xl text-black">{{
+                    i18n("subtitle")
+                  }}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-1/2">
+        <div class="flex justify-end">
+          <select
+            v-if="currentLang"
+            v-model="currentLang"
+            class="mr-5 mt-2"
+            name="lang"
+            @change="updateLang"
+          >
+            <option
+              v-for="(lang, index) in langOptions"
+              :key="index"
+              :value="lang.id"
+              :selected="currentLang == lang.id"
+            >
+              {{ lang.label }}
+            </option>
+          </select>
+        </div>
+      </div>
+    </div>
+    <div class="screen-part2 bg-base"></div>
   </div>
 </template>
 
@@ -68,3 +128,15 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped>
+.screen-part1 {
+  height: 120vh;
+  width: 100%;
+}
+
+.screen-part2 {
+  height: 160vh;
+  width: 100%;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  // TODO:
   purge: [
     "./components/**/*.{vue,js}",
     "./layouts/**/*.vue",
@@ -10,7 +9,29 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      primary: "#FF9100",
+      base: "#EAEAEA",
+      white: "#FFFFFF",
+      whiteShadow: "#C1C1C1",
+    },
+    borderColor: {
+      primary: "#FF9100",
+      base: "#EAEAEA",
+      white: "#FFFFFF",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    textColor: {
+      black: "#1E1E1E",
+    },
+    extend: {
+      borderRadius: {
+        "5xl": "5rem",
+      },
+    },
   },
   variants: {
     extend: {},
