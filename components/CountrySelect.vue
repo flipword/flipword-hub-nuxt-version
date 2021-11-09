f161f187ebfa9a57b5c2d43fd4ab7b96e87569d6
Croquelois Adrien <croquelois.adrien@gmail.com>
2021-11-09 23:52:28 +0100

    :bug: fix country flag path

--- components/CountrySelect.vue
+++ components/CountrySelect.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="flex flex-row gap-2">
     <div class="flex flex-col pt-2">
-      <img
-        :src="`~/assets/images/flags/${flagPath()}`"
-        style="height: 100%; width: 40px"
-      />
+      <img :src="`/flags/${flagPath}`" style="height: 100%; width: 40px" />
     </div>
     <select
       v-if="currentLang"
@@ -25,7 +22,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "vue";
+import { defineComponent, ref } from "vue";
 import { langOptions, flagPaths } from "~/plugins/i18n";
 
 export default defineComponent({
@@ -35,7 +32,7 @@ export default defineComponent({
   },
   emits: ["changeLang"],
   setup(props, { emit }) {
-    const flagPath = reactive(() => flagPaths[props.currentLang]);
+    const flagPath = ref(flagPaths[props.currentLang]);
     const updateLang = (event: any) => {
       emit("changeLang", event.target.value);
     };
