f1172295c0595229f6c73f9c482e7c9721fb380c
croquelois <croquelois.adrien@gmail.com>
2022-01-03 22:32:18 +0100

    :bug: fix web worker declaration client side

--- assets/worker_scripts/addingPopupWorker.js
+++ assets/worker_scripts/addingPopupWorker.js
@@ -1,36 +1,40 @@
-const workerCode = () => {
-  const loop = () => {
-    // Display native word
-    postMessage(1);
-    delay(2000);
-    // Loading trad
-    postMessage(2);
-    delay(2000);
-    // Display foreign word
-    postMessage(3);
-    delay(1000);
-    // Save
-    postMessage(4);
-    delay(500);
-    // Display Notify
-    postMessage(5);
-    delay(3000);
-    // Reload
-    postMessage(6);
-    delay(2000);
+let blobScriptUrl = null;
+
+if (process.client) {
+  const workerCode = () => {
+    const loop = () => {
+      // Display native word
+      postMessage(1);
+      delay(2000);
+      // Loading trad
+      postMessage(2);
+      delay(2000);
+      // Display foreign word
+      postMessage(3);
+      delay(1000);
+      // Save
+      postMessage(4);
+      delay(500);
+      // Display Notify
+      postMessage(5);
+      delay(3000);
+      // Reload
+      postMessage(6);
+      delay(2000);
+    };
+
+    const delay = (millis) => {
+      const now = Date.now();
+      while (Date.now() < now + millis) {}
+    };
+
+    if (typeof loop != "undefined") setInterval(loop, 1);
   };
 
-  const delay = (millis) => {
-    const now = Date.now();
-    while (Date.now() < now + millis) {}
-  };
-
-  if (typeof loop != "undefined") setInterval(loop, 1);
-};
-
-let code = workerCode.toString();
-code = code.substring(code.indexOf("{") + 1, code.lastIndexOf("}"));
-const blobScript = new Blob([code], { type: "application/javascript" });
-const blobScriptUrl = URL.createObjectURL(blobScript);
+  let code = workerCode.toString();
+  code = code.substring(code.indexOf("{") + 1, code.lastIndexOf("}"));
+  const blobScript = new Blob([code], { type: "application/javascript" });
+  blobScriptUrl = URL.createObjectURL(blobScript);
+}
 
 export default blobScriptUrl;
--- assets/worker_scripts/extensionPromoWorker.js
+++ assets/worker_scripts/extensionPromoWorker.js
@@ -1,36 +1,39 @@
-const workerCode = () => {
-  const loop = () => {
-    // Init
-    postMessage(1);
-    delay(2000);
-    // Display cursor
-    postMessage(2);
-    delay(1000);
-    // Start selection
-    postMessage(3);
-    delay(2000);
-    // End selection
-    postMessage(4);
-    delay(500);
-    // Logo appear
-    postMessage(5);
-    delay(1000);
-    // Popup appear
-    postMessage(6);
-    delay(3000);
+let blobScriptUrl = null;
+
+if (process.client) {
+  const workerCode = () => {
+    const loop = () => {
+      // Init
+      postMessage(1);
+      delay(2000);
+      // Display cursor
+      postMessage(2);
+      delay(1000);
+      // Start selection
+      postMessage(3);
+      delay(2000);
+      // End selection
+      postMessage(4);
+      delay(500);
+      // Logo appear
+      postMessage(5);
+      delay(1000);
+      // Popup appear
+      postMessage(6);
+      delay(3000);
+    };
+
+    const delay = (millis) => {
+      const now = Date.now();
+      while (Date.now() < now + millis) {}
+    };
+
+    if (typeof loop != "undefined") setInterval(loop, 1);
   };
 
-  const delay = (millis) => {
-    const now = Date.now();
-    while (Date.now() < now + millis) {}
-  };
-
-  if (typeof loop != "undefined") setInterval(loop, 1);
-};
-
-let code = workerCode.toString();
-code = code.substring(code.indexOf("{") + 1, code.lastIndexOf("}"));
-const blobScript = new Blob([code], { type: "application/javascript" });
-const blobScriptUrl = URL.createObjectURL(blobScript);
-
+  let code = workerCode.toString();
+  code = code.substring(code.indexOf("{") + 1, code.lastIndexOf("}"));
+  const blobScript = new Blob([code], { type: "application/javascript" });
+  blobScriptUrl = URL.createObjectURL(blobScript);
+}
 export default blobScriptUrl;
