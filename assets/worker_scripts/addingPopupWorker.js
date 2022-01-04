8d5d08729cea825a885a22ad4589e67cc0277e65
acroquelois <acroquelois@WAITADCP02W.entreprise.access-it.net>
2022-01-04 17:45:13 +0100

    :fire: delete code

--- assets/worker_scripts/addingPopupWorker.js
+++ assets/worker_scripts/addingPopupWorker.js
@@ -4,31 +4,38 @@ if (process.client) {
   const workerCode = () => {
     const loop = () => {
       // Display native word
-      postMessage(1);
-      delay(2000);
+      const msg1 = () => {
+        postMessage(1);
+        setTimeout(msg2, 2000);
+      };
       // Loading trad
-      postMessage(2);
-      delay(2000);
+      const msg2 = () => {
+        postMessage(2);
+        setTimeout(msg3, 2000);
+      };
       // Display foreign word
-      postMessage(3);
-      delay(1000);
+      const msg3 = () => {
+        postMessage(3);
+        setTimeout(msg4, 1000);
+      };
       // Save
-      postMessage(4);
-      delay(500);
+      const msg4 = () => {
+        postMessage(4);
+        setTimeout(msg5, 500);
+      };
       // Display Notify
-      postMessage(5);
-      delay(3000);
+      const msg5 = () => {
+        postMessage(5);
+        setTimeout(msg6, 3000);
+      };
       // Reload
-      postMessage(6);
-      delay(2000);
+      const msg6 = () => {
+        postMessage(6);
+        setTimeout(msg1, 2000);
+      };
+      msg1();
     };
-
-    const delay = (millis) => {
-      const now = Date.now();
-      while (Date.now() < now + millis) {}
-    };
-
-    if (typeof loop != "undefined") setInterval(loop, 1);
+    if (typeof loop != "undefined") loop();
   };
 
   let code = workerCode.toString();
