6bdf16ac6b66c415e93a73be0d1073b83e8d35db
acroquelois <acroquelois@WAITADCP02W.entreprise.access-it.net>
2022-01-04 17:45:13 +0100

    :bug: fix worker script build

--- assets/worker_scripts/extensionPromoWorker.js
+++ assets/worker_scripts/extensionPromoWorker.js
@@ -4,31 +4,39 @@ if (process.client) {
   const workerCode = () => {
     const loop = () => {
       // Init
-      postMessage(1);
-      delay(2000);
+      const msg1 = () => {
+        postMessage(1);
+        setTimeout(msg2, 2000);
+      };
       // Display cursor
-      postMessage(2);
-      delay(1000);
+      const msg2 = () => {
+        postMessage(2);
+        setTimeout(msg3, 1000);
+      };
       // Start selection
-      postMessage(3);
-      delay(2000);
+      const msg3 = () => {
+        postMessage(3);
+        setTimeout(msg4, 2000);
+      };
       // End selection
-      postMessage(4);
-      delay(500);
+      const msg4 = () => {
+        postMessage(4);
+        setTimeout(msg5, 500);
+      };
       // Logo appear
-      postMessage(5);
-      delay(1000);
+      const msg5 = () => {
+        postMessage(5);
+        setTimeout(msg6, 1000);
+      };
       // Popup appear
-      postMessage(6);
-      delay(3000);
+      const msg6 = () => {
+        postMessage(6);
+        setTimeout(msg1, 3000);
+      };
+      msg1();
     };
 
-    const delay = (millis) => {
-      const now = Date.now();
-      while (Date.now() < now + millis) {}
-    };
-
-    if (typeof loop != "undefined") setInterval(loop, 1);
+    if (typeof loop != "undefined") loop();
   };
 
   let code = workerCode.toString();
