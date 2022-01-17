cadb968be15b6f4f1507f596f5811612b1d96f79
croquelois <croquelois.adrien@gmail.com>
2022-01-17 22:33:55 +0100

    :sparkles: add about us page

---
+++ components/Profil.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <div
+      class="h-14 flex flex-row w-full pl-3 gap-2 justify-start items-center bg-primary filter drop-shadow-md cursor-pointer"
+      @click="router.push('/')"
+    >
+      <img src="assets/icons/logo.png" class="h-10 w-auto" />
+      <span class="text-2xl text-black font-bold">FlipWord</span>
+    </div>
+    <div class="flex flex-row w-full justify-center mt-32">
+      <div class="flex flex-col items-center w-80">
+        <div
+          class="h-52 w-52 bg-primary rounded-full overflow-hidden border-primary border-4 z-20"
+        >
+          <img :src="props.picture_path" />
+        </div>
+        <div
+          class="flex flex-col w-full rounded-lg bg-primary px-5 pt-8 pb-4 -mt-8 gap-4"
+        >
+          <span class="text-center font-bold">{{ props.name }}</span>
+          <span>{{ props.description }}</span>
+          <div class="flex flex-row w-full justify-end gap-2">
+            <img
+              class="h-8 w-auto cursor-pointer"
+              src="assets/images/twitter.png"
+              @click="redirectToSocialNetwork(socialNetwork.Twitter)"
+            />
+            <img
+              class="h-8 w-auto cursor-pointer"
+              src="assets/images/linkedin.png"
+              @click="redirectToSocialNetwork(socialNetwork.Linkedin)"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useNuxtApp } from "#app";
+
+enum SocialNetwork {
+  Linkedin,
+  Twitter,
+}
+
+export default defineComponent({
+  name: "Profil",
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    picture_path: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const { $router } = useNuxtApp();
+
+    const redirectToSocialNetwork = (socialNetwork: SocialNetwork) => {
+      let redirectUrl = "";
+      switch (socialNetwork) {
+        case SocialNetwork.Linkedin:
+          redirectUrl = "https://www.linkedin.com/in/adrien-croquelois-ab065a143";
+          break;
+        case SocialNetwork.Twitter:
+          redirectUrl = "https://twitter.com/a_croquelois";
+          break
+      }
+      if(redirectUrl){
+        window.open(redirectUrl)
+      }
+    };
+    return {
+      props,
+      router: $router,
+      socialNetwork: SocialNetwork,
+      redirectToSocialNetwork
+    };
+  },
+});
+</script>
+
+<style scoped></style>
--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,10 +1,21 @@
-<template><div>About us</div></template>
+<template>
+  <div>
+    <Profil
+      name="Adrien Croquelois"
+      description="Hello there, I'm 23 and I love building quality software. "
+      picture_path="assets/pictures/croquelois.jpg"
+    />
+  </div>
+</template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
+import Profil from "~/components/Profil.vue";
 export default defineComponent({
   name: "AboutUs",
+  components: {
+    Profil,
+  },
   setup() {},
 });
 </script>
