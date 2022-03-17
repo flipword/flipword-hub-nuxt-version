98347030b83ff92bee653ef052bad2a4724ea598
croquelois <croquelois.adrien@gmail.com>
2022-03-17 16:04:07 +0100

    :recycle: refactor profil component

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Layout />
     <Profil
       name="Adrien Croquelois"
       description="Hello there, I'm 23 and I love building quality software. "
@@ -10,10 +11,13 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import Profil from "~/components/Profil.vue";
+import Layout from "~/components/Layout.vue";
+
 export default defineComponent({
   name: "AboutUs",
   components: {
     Profil,
+    Layout,
   },
   setup() {},
 });
