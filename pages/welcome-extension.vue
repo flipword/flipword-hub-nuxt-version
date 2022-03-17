527ea268af1a3fda4751b9c477367059d704587e
croquelois <croquelois.adrien@gmail.com>
2022-03-17 16:03:14 +0100

    :construction: work on extension welcome page

---
+++ pages/welcome-extension.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Layout />
+    <div class="w-full flex flex-row justify-center">
+      <div class="mt-32 w-60 h-52 bg-white"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Layout from "~/components/Layout.vue";
+import { useNuxtApp } from "#app";
+
+export default defineComponent({
+  name: "WelcomeExtension",
+  components: {
+    Layout,
+  },
+  setup() {
+    const { $i18n } = useNuxtApp();
+    return {
+      i18n: $i18n,
+    };
+  },
+});
+</script>
+
+<style scoped></style>
