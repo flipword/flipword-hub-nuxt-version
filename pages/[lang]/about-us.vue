811ce032c6ebd04569efa2f1877e613128000487
acroquelois.eth <49721708+acroquelois@users.noreply.github.com>
2022-03-20 22:40:20 +0100

    :sparkles: add i18n routing (#36)
    * :sparkles: add i18n routing
    
    * :sparkled: add i18n middleware

---
+++ pages/[lang]/about-us.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <Profil
+      name="Adrien Croquelois"
+      :description="i18n('profile_presentation')"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from "vue";
+import Profil from "~/components/Profil.vue";
+import { useNuxtApp, useMeta } from "#app";
+
+export default defineComponent({
+  name: "AboutUs",
+  components: {
+    Profil,
+  },
+  setup() {
+    const { $i18n } = useNuxtApp();
+    const getTitle = () => `- ${$i18n("about_us")}`;
+
+    useMeta({
+      title: computed(() => `Flipword ${$i18n("about_us") ? getTitle() : ""}`),
+    });
+
+    return {
+      i18n: $i18n,
+    };
+  },
+});
+</script>
+
+<style scoped></style>
