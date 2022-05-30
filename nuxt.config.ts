6101e3d4f44a148adf7c5abf0799778e57964582
acroquelois <croquelois.adrien@gmail.com>
2022-05-30 16:42:21 +0200

    :wrench: change firebase function location

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,82 +1,86 @@
-import { defineNuxtConfig } from "nuxt3";
-import { resolve } from "path";
-import { createCommonJS } from "mlly";
+import {defineNuxtConfig} from "nuxt3";
+import {resolve} from "path";
+import {createCommonJS} from "mlly";
 
-const { __dirname } = createCommonJS(import.meta.url);
+const {__dirname} = createCommonJS(import.meta.url);
 
 export default defineNuxtConfig({
-  functions: {
-    location: 'europe-west1',
-  },
-  build: {
-    postcss: {
-      postcssOptions: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
+    firebase: {
+        service: {
+            functions: {
+                location: 'europe-west1',
+            }
+        }
     },
-  },
-  app: {
-    head: {
-      charset: "utf-8",
-      viewport: "width=device-width, initial-scale=1",
-      meta: [
-        {
-          property: "og:title",
-          content: "Flipword",
+    build: {
+        postcss: {
+            postcssOptions: {
+                plugins: {
+                    tailwindcss: {},
+                    autoprefixer: {},
+                },
+            },
         },
-        {
-          property: "og:image",
-          content:
-            "https://storage.googleapis.com/flutter-flip-card.appspot.com/site_thumbnail.png",
-        },
-        {
-          property: "og:description",
-          content: "Learn foreign language vocabulary easily and quickly!",
-        },
-        {
-          property: "og:url",
-          content: "https://flipword.io",
-        },
-        {
-          property: "og:image:width",
-          content: "1200",
-        },
-        {
-          property: "og:image:height",
-          content: "627",
-        },
-        {
-          property: "og:type",
-          content: "website",
-        },
-      ],
-      link: [],
-      style: [],
-      script: [],
     },
-  },
-  // TODO: Wait new method to custom root in Nuxt3
-  hooks: {
-    "pages:extend"(pages) {
-      pages.unshift({
-        name: "home",
-        path: "/",
-        file: resolve(__dirname, "pages/[lang]/index.vue"),
-      });
-      pages.unshift({
-        name: "about-us",
-        path: "/about-us",
-        file: resolve(__dirname, "pages/[lang]/about-us.vue"),
-      });
-      pages.unshift({
-        name: "welcome-extension",
-        path: "/welcome-extension",
-        file: resolve(__dirname, "pages/[lang]/welcome-extension.vue"),
-      });
+    app: {
+        head: {
+            charset: "utf-8",
+            viewport: "width=device-width, initial-scale=1",
+            meta: [
+                {
+                    property: "og:title",
+                    content: "Flipword",
+                },
+                {
+                    property: "og:image",
+                    content:
+                        "https://storage.googleapis.com/flutter-flip-card.appspot.com/site_thumbnail.png",
+                },
+                {
+                    property: "og:description",
+                    content: "Learn foreign language vocabulary easily and quickly!",
+                },
+                {
+                    property: "og:url",
+                    content: "https://flipword.io",
+                },
+                {
+                    property: "og:image:width",
+                    content: "1200",
+                },
+                {
+                    property: "og:image:height",
+                    content: "627",
+                },
+                {
+                    property: "og:type",
+                    content: "website",
+                },
+            ],
+            link: [],
+            style: [],
+            script: [],
+        },
     },
-  },
-  extensions: [".js", ".mjs", ".ts", ".tsx", ".vue", ".json"],
+    // TODO: Wait new method to custom root in Nuxt3
+    hooks: {
+        "pages:extend"(pages) {
+            pages.unshift({
+                name: "home",
+                path: "/",
+                file: resolve(__dirname, "pages/[lang]/index.vue"),
+            });
+            pages.unshift({
+                name: "about-us",
+                path: "/about-us",
+                file: resolve(__dirname, "pages/[lang]/about-us.vue"),
+            });
+            pages.unshift({
+                name: "welcome-extension",
+                path: "/welcome-extension",
+                file: resolve(__dirname, "pages/[lang]/welcome-extension.vue"),
+            });
+        },
+    },
+    extensions: [".js", ".mjs", ".ts", ".tsx", ".vue", ".json"],
 });
